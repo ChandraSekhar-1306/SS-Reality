5f13b32376f56687eefde0096e1a98bdbab12b8f
chandu <cstangirala2003@gmail.com>
2024-12-04 12:34:52 +0530

    completed sign up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,11 +4,9 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-            <Link to='/'>
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>SS</span>
-            <span className='text-slate-700'>Reality</span>
-        </h1>
+        <Link to='/'>
+        <img src='https://res.cloudinary.com/duefzmqm1/image/upload/v1733294283/ss_reality_ticgaq.jpg' className='flex flex-wrap w-32 h-13'/>
+            
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
