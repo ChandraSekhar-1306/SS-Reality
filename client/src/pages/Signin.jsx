364296f13ae552b246d49a21436e1c2f21baa7a4
chandu <cstangirala2003@gmail.com>
2024-12-04 16:02:30 +0530

    added redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import {Link , useNavigate} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import{signInStart, signInSuccess , signInFailure} from '../redux/user/userSlice'
 
 export default function Signin() {
   const[formData , setFormData] = useState({})
-  const[error , setError] = useState(null)
-  const[loading , setLoading] = useState(false)
+ const{loading, error} = useSelector((state)=>state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch()
   const handleChange = (e)=>{
       setFormData({
         ...formData,
@@ -15,7 +17,7 @@ export default function Signin() {
   const handleSubmit = async(e)=>{
       e.preventDefault();
       try {
-        setLoading(true)
+        dispatch(signInStart())
 
       const res = await fetch('/api/auth/signin' , 
         {
@@ -29,17 +31,14 @@ export default function Signin() {
       const data = await res.json();
       console.log(data)
       if(data.success === false){
-        setLoading(false)
-        setError(data.message);
+        dispatch(signInFailure(data.message))
         return;
       }
-      setLoading(false)
-      setError(null)
+     dispatch(signInSuccess(data))
       navigate('/')
 
       } catch (error) {
-        setLoading(false)
-        setError(error.message)
+        dispatch(signInFailure(error.message))
       }
       
 
@@ -52,7 +51,7 @@ export default function Signin() {
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
         <button disabled={loading}  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {loading ? 'Loading...':'Sign ip'}
+          {loading ? 'Loading...':'Sign in'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
