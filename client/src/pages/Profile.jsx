a57b786488032fc465c7cc964cab8c7a0693051f
chandu <cstangirala2003@gmail.com>
2024-12-08 20:30:59 +0530

    sign out functionality is implemented

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,7 +5,7 @@ import {app} from '../firebase.js'
 import { useRef } from 'react'
 import{Modal , Button , Alert} from 'flowbite-react'
 import {HiOutlineExclamationCircle} from 'react-icons/hi'
-import {updateUserStart , updateUserSuccess , updateUserFailure , deleteUserStart , deleteUserSuccess , deleteUserFailure} from '../redux/user/userSlice'
+import {updateUserStart , updateUserSuccess , updateUserFailure , deleteUserStart , deleteUserSuccess , deleteUserFailure, signOutUserStart, signOutUserFailure, signOutUserSuccess} from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 
 
@@ -94,6 +94,21 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async()=>{
+      try {
+        dispatch(signOutUserStart())
+        const res = await fetch('/api/auth/signout');
+        const data = await res.json()
+        if(data.success == false) {
+          dispatch(signOutUserFailure(data.message))
+          return;
+        }
+        dispatch(signOutUserSuccess(data))
+        
+      } catch (error) {
+        dispatch(signOutUserFailure(error.message))
+      }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -118,7 +133,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={()=> setShowModal(true)} className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 font-bold mt-5 text-center'>{error ? error:""}</p>
       <p className='text-green-700 font-bold mt-5 text-center'>{updateSuccess ? 'User is updated successfully!':""}</p>
